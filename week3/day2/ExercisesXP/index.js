bd3ef17a93c2f3f0475f977709ddc26f3938f1df
mawasserman <wasserman.mv@gmail.com>
2023-12-16 22:08:45 +0200

    week3day2 exercisexp start

---
+++ week3/day2/ExercisesXP/index.js
@@ -0,0 +1,12 @@
+// Exercise 1: Your Favorite Food
+
+// Instructions
+// Store your favorite food into a variable.
+var favFood = "Pizza";
+
+// Store your favorite meal of the day into a variable (ie. breakfast, lunch or dinner)
+var favMeal = "second breakfast"
+// I am some kind of hobbit...
+
+// Console.log I eat <favorite food> at every <favorite meal>
+console.log("I eat " + favFood + " at every " +favMeal);
