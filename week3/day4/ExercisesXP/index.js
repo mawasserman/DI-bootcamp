c9cba2ce997794e562626cd058169a5671004617
mawasserman <wasserman.mv@gmail.com>
2023-12-17 01:04:59 +0200

    week3day4 dailychallenge done

--- week3/day4/ExercisesXP/index.js
+++ week3/day4/ExercisesXP/index.js
@@ -71,7 +71,7 @@ if (users.length == 0){
 }
 // If there is 1 user, console.log “<name_user> is online”.
 else if (users.length == 1){
-    console.log( users[0] + " is online”);
+    console.log(users[0] + " is online”);
 }
 // If there are 2 users, console.log “<name_user1> and <name_user2> are online”.
 else if (users.length == 2){
