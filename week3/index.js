3482692a35a3614d48a379137d86c5ad4c70888f
mawasserman <wasserman.mv@gmail.com>
2023-12-17 18:00:00 +0200

    week3day6 daily done

---
+++ week3/index.js
@@ -0,0 +1,23 @@
+// Instructions
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+// Do this Daily Challenge by yourself, without looking at the answers on the internet.
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * *
+// * * * * * *
+
+// DID NOT WORK, saw explanation but left the wrong one here to understand
+let ast = "*"
+for (i = "*"; ast.lenght < 7; i=i+"*"){
+    ast = ast + "*";
+    console.log(ast);
+}
+
+// nested loops
+while (ast.length < 7) {
+    for (let i = 0; ast. < 9; i++) {
+    console.log(i);
+}
