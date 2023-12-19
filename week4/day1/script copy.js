700fe86e574900c60913d0389d918eff62299a32
mawasserman <wasserman.mv@gmail.com>
2023-12-19 23:18:35 +0200

    starting thre ev 2 day

---
+++ week4/day1/script copy.js
@@ -0,0 +1,43 @@
+// for(let i = 0; i <= 6; i++) {
+//     let star = ""
+//     for(let x = 0; x <= i; x++){
+//         // console.log(i , x);
+//         star += "* ";
+//     }
+//     console.log(star);
+// }
+
+// [1][0][2] = [0][0] = [1][1] = [2];
+
+// const numbers = [5, 0, 9, 1, 7, 4, 2, 6, 3, 8];
+
+// console.log(numbers);
+
+// for (let i = 0; i < numbers.length; i++) {
+//   for (let j = 0; j < numbers.length; j++) {
+//     if (numbers[i] > numbers[j]) {
+//       let temp = numbers[i];
+//       numbers[i] = numbers[j];
+//       numbers[j] = temp;
+//     }
+//   }
+// }
+
+// console.log(numbers);
+
+// let x = 0
+// while(x < 4){
+//     x++
+//     console.log(x);
+// }
+// let x = 0
+// while(x < 0){
+//     x++;
+//   console.log('while',x);
+// }
+
+// do {
+//   x++;
+//   console.log('do while=>',x);
+// }
+// while(x < 0)
---
+++ week4/day2/DailyChallenge/script.js
@@ -0,0 +1,16 @@
+// Instructions
+// Prompt the user for several words (separated by commas).
+
+// Put the words into an array. 
+
+// Console.log the words one per line, in a rectangular frame as seen below.
+
+// Check out the Hints and Requirements below.
+// For example, if the user gives you:
+
+// Hello, World, in, a, frame
+// you will transform it to : ["Hello", "World", "in", "a", "frame"]
+
+
+// Hint
+// The number of stars that wraps the sentence, must depend on the length of the longest word.
