8a46992bee1e649dd201ee258ec73901715affcc
mawasserman <wasserman.mv@gmail.com>
2023-12-22 09:47:32 +0200

    w3d1e3

--- week3/day1/exercises/script.js
+++ week3/day1/exercises/script.js
@@ -28,3 +28,16 @@ let futureAge = futureYear - bdayYear
 
 // 4. Display : "I will be NN in YYYY", substituting the values.
 console.log("I will be "+ futureAge + " in " + futureYear)
+
+// Exercise 3
+// 1. Create a numerically indexed table (ie. an array): pets, like this ['cat','dog','fish','rabbit','cow']
+let pets = ['cat','dog','fish','rabbit','cow']
+
+// 2. Display dog
+console.log(pets[1])
+
+// 3. Add to the array pets, the pet horse. Remove the pet rabbit
+pets[3]='horse'
+
+// 4. Display the array length
+console.log(pets.length)
