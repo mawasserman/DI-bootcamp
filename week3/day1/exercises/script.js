fa64b76fc190cee8cdfec8f85b660eb924bfd14f
mawasserman <wasserman.mv@gmail.com>
2023-12-22 08:50:03 +0200

    review exercises week 3

--- week3/day1/exercises/script.js
+++ week3/day1/exercises/script.js
@@ -7,11 +7,11 @@ let addressStreet = 'Rothshild'
 let country= 'Israel'
 
 // 2. Write a variable named globalAddress, and concatenate inside, the variables:
-let globalAddress = "I live in " + addressNumber + " " + addressStreet + " " + country;
+let globalAddress = "I live in " + addressStreet + " " + addressNumber + ", in" + country;
 
 // addressNumber
 // addressStreet
 // country
 // In order to create a sentence
 // 3. Display globalAddress Example: globalAddress should display « I live in BenYehuda 5, in Israel »
-console.log(globalAddress)
+console.log(globalAddress);
