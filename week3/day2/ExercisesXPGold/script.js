e9878c48ec41d4df40ec2d87a64bbf5848c73f91
mawasserman <wasserman.mv@gmail.com>
2023-12-22 11:32:38 +0200

    Week3Day2 Exercises XPGold done

--- week3/day2/ExercisesXPGold/script.js
+++ week3/day2/ExercisesXPGold/script.js
@@ -27,18 +27,6 @@ let finalString = newStr1 + " " + newStr2;
 // 4. Finally console.log the new concatenated string.
 console.log(finalString);
 
-
-// Some Examples
-
-// let str1 = "mix";
-// let str2 = "pod";
-// let newWord should be equal to 'pox mid'
-
-// let firstWord = "Hello";
-// let secondWord = "World";
-// let thirdWord should be equal to 'Wollo Herld'
-
-
 // Exercise 3 : Calculator
 // Instructions
 // Make a Calculator. Follow the instructions:
@@ -46,7 +34,20 @@ console.log(finalString);
 // Prompt the user for the first number.
 // Store the first number in a variable called num1.
 // Hint : console.log the type of the variable num1. What should you do to convert it to a number ?
+let num1 = prompt("Give me a number");
+
 // Prompt the user for the second number.
 // Store the second number in a variable called num2.
+let num2 = prompt("Give me another number");
+
 // Create an Alert where the value is the SUM of num1 and num2.
+// let sum = Number(num1) + Number(num2);
+// alert("The sum is " + sum);
+
 // BONUS: Make a program that can subtract, multiply, and also divide!
+
+let sum = Number(num1) + Number(num2);
+let sub = Number(num1) - Number(num2);
+let mul = Number(num1) * Number(num2);
+let divi = Number(num1) / Number(num2);
+alert("The sum is " + sum + ", the subtraction is " + sub + ", the multiplication is " + mul + ", and the division is " + divi);
