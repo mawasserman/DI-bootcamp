6c5286d96b6fc7f1a00713b06e426f1839eef019
mawasserman <wasserman.mv@gmail.com>
2023-12-27 22:02:44 +0200

    bedtime commit]

---
+++ week5/day4/OnlineLearning/script.js
@@ -0,0 +1,14 @@
+var css = document.querySelector("h3")
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+color1.addEventListener(ïnput", function (){
+    console.log(color1.value)
+})
+// console.log
+
+color2.addEventListener(ïnput", function (){
+    console.log(color2.value)
+})
+
+// quer mudar a cor de fundo
