ef05daaa1f078cb07c5bba99e215749c7b57115a
mawasserman <wasserman.mv@gmail.com>
2023-12-28 21:55:08 +0200

    gradient background

--- week5/day4/OnlineLearning/script.js
+++ week5/day4/OnlineLearning/script.js
@@ -1,13 +1,27 @@
-var css = document.querySelector("h3")
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
+let css = document.querySelector("h3"); // step 1
+let color1 = document.querySelector(".color1"); //1
+let color2 = document.querySelector(".color2"); //1
+let body = document.getElementById("gradient"); // pq a cor está aplicada no body, ai cria a variavel pra poder mudar ela usando dom
 
-color1.addEventListener("input", function (){
-    console.log(color1.value)})
-// console.log
+function setGradient(){ 
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-color2.addEventListener("input", function (){
-    console.log(color2.value)
-})
+    css.textContent = body.style.background + ";";
+} // step 4
+color1.addEventListener("input", setGradient)
+ // console.log(color1.value)}) // step 2
+// console.log as cores enquanto eu tô passando em cima delas para decidir ainda// step 3
 
-// quer mudar a cor de fundo
+color2.addEventListener("input", setGradient) //nãp pode colocar o () pq o addEventListener ja vai executar a função automaticamente daria erro
+// function (){
+    // console.log(color2.value) })//2
+//      body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+// });
+   
+
+// quer mudar a cor de fundo para as cores escolhidas
+
+//addEvent mantem o css no arquivo css.. mas poderia fazer adicionando oniput = "setGradient" na tag do input... a diferença é que ta adicionando ao HTML e limita ele
+
+
+//como pegar ele pra usar agora --- step 5 que tá lá em cima
