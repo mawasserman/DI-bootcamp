acfb54917b2f32b70110db736ac0f4c5d5a2ec1f
mawasserman <wasserman.mv@gmail.com>
2023-12-31 18:58:09 +0200

    exercises xp week 3 day 6

--- week3/day6/ExercisesXP/script.js
+++ week3/day6/ExercisesXP/script.js
@@ -1,59 +1,77 @@
-// Exercise 1 : List Of People
-// Instructions
-const people = ["Greg", "Mary", "Devon", "James"];
+// // Exercise 1 : List Of People
+// // Instructions
+// const people = ["Greg", "Mary", "Devon", "James"];
 
 
 // Part I - Review About Arrays
-// Write code to remove “Greg” from the people array.
-const noGreg = people.shift();
+// // Write code to remove “Greg” from the people array.
+// const noGreg = people.shift();
 
-// Write code to replace “James” to “Jason”.
-people[2] = 'Jason';
+// // Write code to replace “James” to “Jason”.
+// people[2] = 'Jason';
 
-// Write code to add your name to the end of the people array.
-people.push('Marcella');
-console.log(people);
+// // Write code to add your name to the end of the people array.
+// people.push('Marcella');
+// console.log(people);
 
-// Write code that console.logs Mary’s index. take a look at the indexOf method on Google.
-const maryIndex = people.indexOf('Mary')
-console.log(maryIndex)
+// // Write code that console.logs Mary’s index. take a look at the indexOf method on Google.
+// const maryIndex = people.indexOf('Mary')
+// console.log(maryIndex)
 
-// Write code to make a copy of the people array using the slice method.
-// The copy should NOT include “Mary” or your name.
-// Hint: remember that now the people array should look like this const people = ["Mary", "Devon", "Jason", "Yourname"];
-// Hint: Check out the documentation for the slice method
-const copyPeople = people.slice(1, 3)
-console.log(copyPeople);
+// // Write code to make a copy of the people array using the slice method.
+// // The copy should NOT include “Mary” or your name.
+// // Hint: remember that now the people array should look like this const people = ["Mary", "Devon", "Jason", "Yourname"];
+// // Hint: Check out the documentation for the slice method
+// const copyPeople = people.slice(1, 3)
+// console.log(copyPeople);
 
-// Write code that gives the index of “Foo”. Why does it return -1 ? BECAUSE THERE IS NO FOO ON THE ARRAY
-console.log(people.indexOf('Foo')) 
+// // Write code that gives the index of “Foo”. Why does it return -1 ? BECAUSE THERE IS NO FOO ON THE ARRAY
+// console.log(people.indexOf('Foo')) 
 
-// Create a variable called last which value is the last element of the array.
-// Hint: What is the relationship between the index of the last element in the array and the length of the array?
-const last = copyPeople[copyPeople.length - 1];
-console.log(last);
+// // Create a variable called last which value is the last element of the array.
+// // Hint: What is the relationship between the index of the last element in the array and the length of the array?
+// const last = copyPeople[copyPeople.length - 1];
+// console.log(last);
 
 
-// Part II - Loops
-// Using a loop, iterate through the people array and console.log each person.
-
+// // Part II - Loops
+// // Using a loop, iterate through the people array and console.log each person.
+// for ( let person of people)
+// console.log (person);
 // Using a loop, iterate through the people array and exit the loop after you console.log “Devon” .
 // Hint: take a look at the break statement in the lesson.
+// for ( let persona of people)
+// if ( persona == 'Devon'){
+//     break;
+// }else {
+// console.log (persona);
+// }
 
 
 // 🌟 Exercise 2 : Your Favorite Colors
-// Instructions
-// Create an array called colors where the value is a list of your five favorite colors.
-// Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
-// Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
-// Hint : create an array of suffixes to do the Bonus
+// // Instructions
+// // Create an array called colors where the value is a list of your five favorite colors.
+// const colors = ['green', 'blue', 'pink', 'grey', 'purple']
+// // Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
+// colors.forEach(function(color, i) {
+//     console.log("My #" + (Number(i)+1) +" choice is " + color)
+// })
+// // Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
+// // Hint : create an array of suffixes to do the Bonus
 
 
 // 🌟 Exercise 3 : Repeat The Question
 // Instructions
 // Prompt the user for a number.
 // Hint : Check the data type you receive from the prompt (ie. Use the typeof method)
-
+const numberInput = prompt("give me a number") 
+while ( numberInput < 10){
+    if ( numberInput == isNaN){
+        alert ("This is not a number!")
+        prompt ("Give me a indo-arabic numerals")
+    }
+    else 
+}
 // While the number is smaller than 10 continue asking the user for a new number.
 // Tip : Which while loop is more relevant for this situation?
 
