e5792f87b4970f64ae9b5a9b0ad63e5987fde253
mawasserman <wasserman.mv@gmail.com>
2024-01-01 22:20:18 +0200

    w4d2 daily

--- week4/day2/DailyChallenge/script.js
+++ week4/day2/DailyChallenge/script.js
@@ -1,33 +1,21 @@
-// Instructions
 // Prompt the user for several words (separated by commas).
-let mySentence = prompt("Write a sentence with the words separeted by commas. Ex. I, like, chocolate")
-
-// Put the words into an array. 
-const arrayOfWords = mySentence.split(",")
+let sentence = prompt("Give me several words (separated by commas)")
+// Put the words into an array.
+let sentenceArray = sentence.split(',');
+let newSentence = '**'
 // Console.log the words one per line, in a rectangular frame as seen below.
-arrayOfWords.forEach()
+function banner(){
+  for (let i=0; i<sentenceArray.length; i++)
+  newSentence = newSentence + '*'
+}
 // Check out the Hints and Requirements below.
 // For example, if the user gives you:
 // Hello, World, in, a, frame
 // you will transform it to : ["Hello", "World", "in", "a", "frame"]
+// that will get displayed as:
+
+// stars and words
+
 
 // Hint
 // The number of stars that wraps the sentence, must depend on the length of the longest word.
-function sentenceWithFrame(mySentence) {
-    const sentenceLength = 
-  
-    const horizontalLine = '+' + '-'.repeat(maxLength + 2) + '+';
-    console.log(horizontalLine);
-  
-    words.forEach(word => {
-      const spaces = ' '.repeat(maxLength - word.length);
-      console.log(`| ${word}${spaces} |`);
-    });
-  
-    console.log(horizontalLine);
-  }
-  
-  const userInput = prompt("Enter words separated by commas:");
-  const wordsArray = userInput.split(',').map(word => word.trim());
-  
-  createFrame(wordsArray);
