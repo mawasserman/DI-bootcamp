f2fd8563a98bdbf16709741b3f473966c47e6e3a
mawasserman <wasserman.mv@gmail.com>
2024-01-06 21:35:21 +0200

    studying database

---
+++ week4/day3/script.js
@@ -0,0 +1,23 @@
+// </html>
+// For each of the questions, find 2 WAYS of accessing :
+
+// 1. The div DOM node?
+// body.firstElementChild
+// body.children[0]
+
+// 2. The ul DOM node?
+
+
+// 3. The second li (with Pete)?Accessing the div DOM node:
+
+// CHAT GPT
+//  By using document.querySelector('div').
+// By using document.getElementsByTagName('div')[0].
+// Accessing the ul DOM node:
+
+// By using document.querySelector('ul').
+// By using document.getElementsByTagName('ul')[0].
+// Accessing the second li (with Pete):
+
+// By using document.querySelectorAll('li')[1].
+// By using document.getElementsByTagName('li')[1].
