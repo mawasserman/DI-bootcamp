495976f01f7785090368831549934ab036f9c8e7
mawasserman <wasserman.mv@gmail.com>
2024-01-08 23:22:38 +0200

    week7day2 dailyexercise done

---
+++ week7/day2/DailyChallenge/DailyChallenge.sql
@@ -0,0 +1,54 @@
+-- Queries
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- )
+
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar')
+
+-- SELECT * FROM FirstTab
+
+-- CREATE TABLE SecondTab (
+--     id integer 
+-- )
+
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL)
+
+
+-- SELECT * FROM SecondTab
+---------------
+-- Q1. What will be the OUTPUT of the following statement?
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+
+-- -- 3 (5, 6 and 7)
+-- -- I was wrong, it was 0.. because you can not compare anything to null
+
+
+-- Q2. What will be the OUTPUT of the following statement?
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+-- -- 2, because null is nul, so the 6 and 7 
+-- -- yeah!
+
+-- Q3. What will be the OUTPUT of the following statement?
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+-- -- 3 because of the no
+-- -- Wrong again... the null ends the whole thing and that'd it
+
+-- Q4. What will be the OUTPUT of the following statement?
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+-- -- 6 and 7
+-- -- Got it right!! Because I finally learn that I can't compare anything to null
