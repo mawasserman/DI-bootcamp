fc8b5552d8be39cf0567c7c7b611132cecb9ead4
mawasserman <wasserman.mv@gmail.com>
2024-01-10 23:27:25 +0200

    going to sleep

--- week7/day4/ExercisesXP/exercise1.sql
+++ week7/day4/ExercisesXP/exercise1.sql
@@ -1,23 +1,59 @@
 -- Exercise 1: DVD Rental
 -- Instructions
 -- Get a list of all the languages, from the language table.
+select name from language;
 
 -- Get a list of all films joined with their languages – select the following details : film title, description, and language name.
+select film.title, film.description, language.name
+from film
+inner join language
+on film.language_id = language.language_id;
 
 -- Get all languages, even if there are no films in those languages – select the following details : film title, description, and language name.
+select film.title, film.description, language.name
+from film
+right outer join language
+on film.language_id = language.language_id;
 
 -- Create a new table called new_film with the following columns : id, name. Add some new films to the table.
+create table new_film(
+id serial primary key,
+name varchar (500)
+)
+
+insert into new_film (name)
+values('Die hard'),
+('Mortal kombat'),
+('Titanic'),
+('Se eu fosse você');
 
 -- Create a new table called customer_review, which will contain film reviews that customers will make.
+create table customer_review(
+
 -- Think about the DELETE constraint: if a film is deleted, its review should be automatically deleted.
 -- It should have the following columns:
 -- review_id – a primary key, non null, auto-increment.
+review_id serial primary key non null,
+review varchar (500)
+)
+
 -- film_id – references the new_film table. The film that is being reviewed.
+film_id integer referencer new_film(id),
+
 -- language_id – references the language table. What language the review is in.
+language_id integer references language(language_id),
+
 -- title – the title of the review.
+title varchar (500),
+
 -- score – the rating of the review (1-10).
+score integer check (score between 1 and 10),
+
 -- review_text – the text of the review. No limit on the length.
+review_text text
+
 -- last_update – when the review was last updated.
+last_update timestamp default current_timestamp
 
 -- Add 2 movie reviews. Make sure you link them to valid objects in the other tables.
 
