9b8e33150bd7756a1ecaf1d2ff2ce9783b2fa295
mawasserman <wasserman.mv@gmail.com>
2024-01-11 22:19:58 +0200

    ex1

--- week7/day4/ExercisesXP/exercise1.sql
+++ week7/day4/ExercisesXP/exercise1.sql
@@ -29,16 +29,13 @@ values('Die hard'),
 
 -- Create a new table called customer_review, which will contain film reviews that customers will make.
 create table customer_review(
-
--- Think about the DELETE constraint: if a film is deleted, its review should be automatically deleted.
+	
 -- It should have the following columns:
 -- review_id – a primary key, non null, auto-increment.
-review_id serial primary key non null,
-review varchar (500)
-)
+review_id serial primary key not null,
 
 -- film_id – references the new_film table. The film that is being reviewed.
-film_id integer referencer new_film(id),
+film_id integer,
 
 -- language_id – references the language table. What language the review is in.
 language_id integer references language(language_id),
@@ -50,11 +47,21 @@ title varchar (500),
 score integer check (score between 1 and 10),
 
 -- review_text – the text of the review. No limit on the length.
-review_text text
+review_text text,
 
 -- last_update – when the review was last updated.
-last_update timestamp default current_timestamp
+last_update timestamp default current_timestamp,
+	
+-- Think about the DELETE constraint: if a film is deleted, its review should be automatically deleted.
+FOREIGN KEY (film_id) REFERENCES new_film(id)
+);
+
+select * from customer_review;
 
 -- Add 2 movie reviews. Make sure you link them to valid objects in the other tables.
+	INSERT INTO customer_review (film_id, language_id, title, score, review_text)
+VALUES
+    (1, 1, 'Die Hard', 9, 'The movie had amazing action sequences...'),
+    (3, 1, 'Titanic', 8, 'The storytelling was compelling...');
 
 -- Delete a film that has a review from the new_film table, what happens to the customer_review table?
