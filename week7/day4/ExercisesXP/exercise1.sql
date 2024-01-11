1d1eb93962d5137d661f46995d72cba2283a86d0
mawasserman <wasserman.mv@gmail.com>
2024-01-11 22:51:57 +0200

    mimi

--- week7/day4/ExercisesXP/exercise1.sql
+++ week7/day4/ExercisesXP/exercise1.sql
@@ -65,3 +65,33 @@ VALUES
     (3, 1, 'Titanic', 8, 'The storytelling was compelling...');
 
 -- Delete a film that has a review from the new_film table, what happens to the customer_review table?
+delete from new_film where id = 1;
+
+-- ERROR:  Key (id)=(1) is still referenced from table "customer_review".update or delete on table "new_film" violates foreign key constraint "customer_review_film_id_fkey" on table "customer_review" 
+
+-- ERROR:  update or delete on table "new_film" violates foreign key constraint "customer_review_film_id_fkey" on table "customer_review"
+-- SQL state: 23503
+-- Detail: Key (id)=(1) is still referenced from table "customer_review".
+
+-----------------------
+
+--  Exercise 2 : DVD Rental
+-- Instructions
+-- Use UPDATE to change the language of some films. Make sure that you use valid languages.
+
+-- Which foreign keys (references) are defined for the customer table? How does this affect the way in which we INSERT into the customer table?
+
+-- We created a new table called customer_review. Drop this table. Is this an easy step, or does it need extra checking?
+
+-- Find out how many rentals are still outstanding (ie. have not been returned to the store yet).
+
+-- Find the 30 most expensive movies which are outstanding (ie. have not been returned to the store yet)
+
+-- Your friend is at the store, and decides to rent a movie. He knows he wants to see 4 movies, but he can’t remember their names. Can you help him find which movies he wants to rent?
+-- The 1st film : The film is about a sumo wrestler, and one of the actors is Penelope Monroe.
+
+-- The 2nd film : A short documentary (less than 1 hour long), rated “R”.
+
+-- The 3rd film : A film that his friend Matthew Mahan rented. He paid over $4.00 for the rental, and he returned it between the 28th of July and the 1st of August, 2005.
+
+-- The 4th film : His friend Matthew Mahan watched this film, as well. It had the word “boat” in the title or description, and it looked like it was a very expensive DVD to replace.
