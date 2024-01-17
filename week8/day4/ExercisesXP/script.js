c4a453c49d6fd03de0eecd14489da70cddd93587
mawasserman <wasserman.mv@gmail.com>
2024-01-17 21:31:49 +0200

    week8day4 exercisesXP done

--- week8/day4/ExercisesXP/script.js
+++ week8/day4/ExercisesXP/script.js
@@ -82,4 +82,9 @@ const students = [{name: "Ray", course: "Computer Science", isPassed: true},
                {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
                {name: "Jamie", course: "Big Data", isPassed: false}];
 // Using the filter() method, create a new array, containing the students that passed the course.
+const passed = students.filter(student => student.isPassed == true);
+console.log(passed);
+
 // Bonus : Chain the filter method with a forEach method, to congratulate the students with their name and course name (ie. “Good job Jenner, you passed the course in Information Technology”, “Good Job Marco you passed the course in Robotics” ect…)
+const congratulate = passed.forEach(student => console.log(`Good job ${passed.name}, you passed the course in ${passed.course}`)); 
+// is getting undefined 
