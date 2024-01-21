59226c37870eced2fa435af72ba29f196578146f
mawasserman <wasserman.mv@gmail.com>
2024-01-21 19:50:05 +0200

    exercicios de aula

---
+++ week9/day1/script.js
@@ -0,0 +1,26 @@
+// exercicios feitos em aula
+
+
+
+
+
+
+
+
+// filter for all users older than 30
+// and store their data in an array
+// use Array.filter Array.map Object.keys
+
+const users = {
+    user1: { age: 44, name: "picard" },
+    user2: { age: 12, name: "sisko" },
+    user3: { age: 109, name: "janeway" },
+  };
+  /** 
+  Results:
+  */
+  [
+    {id:'user1',age:44, name: 'picard'},
+    {id:'user3',age:109, name: 'janeway'}
+  ]
+  /*/
