f4d7eb36a1090676e17aa655242704ba3931cafa
mawasserman <wasserman.mv@gmail.com>
2024-02-13 16:24:50 +0200

    week3day2 ExerciseNinja Done

--- week3/day2/ExercisesXPNinja/script.js
+++ week3/day2/ExercisesXPNinja/script.js
@@ -75,34 +75,31 @@ let numUser = Number(inputUser);
 
     // If the number given is less than 2 : return “boom”
     if (numUser <= 2){
-        alert("boom")
+        alert("boom");
     }
 
     // If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
     else{
-        if (numUser%2 == 0 && numUser%5 !== 0){
-            let newBoom = [b, m];
-            for(let i = 0; i < numUser; i++)
-            newBoom.
-
+        let newBoom = ["b", "m"];
+        for(let i = 0; i < numUser; i++){
+            newBoom.splice(1, 0, "o");
         }
-        if (numUser%5 !== 0){
-            alert("boom!")
+        if (numUser%2 == 0){
+            newBoom.push("!");            
+        }    
+        let stringBoom = newBoom.join(""); 
+        if (numUser%5 == 0){
+            alert(stringBoom.toUpperCase());
+        }else{
+            alert(stringBoom);
         }
-        else {
-            alert("BOOM!")
-        }
+        
     }
-        let newBoom = [b, m], 
-        alert("b" + numUser + "m")
-    }
+}
     // If the number given is evenly divisible by 2, add a exclamation mark to the end.
-else 
+ 
 // If the number given is evenly divisible by 5, return the string in ALL CAPS.
-else if (numUser%5 == 0){
-    alert("BOOM")
-}
-}
+
 // If the number given is evenly divisible by both 2 and 5, return the string in ALL CAPS and add an exclamation mark to the end.
 
 // Examples
